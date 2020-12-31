6ae6a9e5674e4176721724da2b1791692a477f17
davahamka <davahamka512@gmail.com>
2020-12-31 10:07:00 +0700

    feat: remove space input

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -3,6 +3,7 @@ import { useState } from "preact/hooks";
 import { infixToPostfix } from "../../helpers/utils";
 import Button from "../../components/button";
 
+
 const Home: FunctionalComponent = () => {
     const [values, setValue] = useState("");
     const [res, setResult] = useState("");
@@ -10,7 +11,13 @@ const Home: FunctionalComponent = () => {
     const handleSubmit = (e?: any) => {
         e.preventDefault();
         setResult(infixToPostfix(values));
-    }
+    };
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+        if (e.key === " ") {
+            e.preventDefault();
+        }
+    };
     return (
         <section class="my-4 p-4">
             <div class="flex flex-col items-center text-center">
@@ -20,19 +27,22 @@ const Home: FunctionalComponent = () => {
                 <p class="text-sm md:text-md mt-2">
                     Enter the infix expression into the box and convert it!
                 </p>
-                <form onSubmit={(e) => handleSubmit(e)}>
-                <div class=" my-4">
-                    <input
-                        type="text"
-                        value={values}
-                        onChange={e =>
-                            setValue(e.currentTarget.value)
-                        }
-                        placeholder=""
-                        class="border border-gray-400 rounded w-full px-2 py-2"
-                    />
-                </div>
-                <Button onClick={() => handleSubmit()}>Convert</Button>
+                <form onSubmit={e => handleSubmit(e)}>
+                    <div class=" my-4">
+                        <input
+                            type="text"
+                            value={values}
+                            onKeyDown={handleKeyDown}
+                            onChange={e =>
+                                setValue(
+                                    e.currentTarget.value.replace(/\s/g, "")
+                                )
+                            }
+                            placeholder=""
+                            class="border border-gray-400 rounded w-full px-2 py-2"
+                        />
+                    </div>
+                    <Button onClick={() => handleSubmit()}>Convert</Button>
                 </form>
                 <p class="my-4 md:text-2xl">
                     Postfix: <span class="font-bold">{res}</span>
